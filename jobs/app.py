2d50a4c21a6b583eca913149dac550cb558dceb1
Bhuvaneswari Ramamurthi <M1023433@mindtree.com>
2020-12-31 17:21:16 +0530

    test

--- jobs/app.py
+++ jobs/app.py
@@ -21,7 +21,7 @@ def execute_sql(sql, values = (), commit = False, single = False):
 	cursor = connection.execute(sql, values)
 	if commit is True:
 		results = connection.commit()
-	else
+	else:
 		results = cursor.fetchone() if single else cursor.fetchall()
 	cursor.close()
 	jobs = results
